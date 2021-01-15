d337473a0b25a3223a12f89bcf1b7f0bce7aa9b9
kuma88 <kumarasond@gmail.com>
2021-01-15 12:07:21 +0800

    added darkmode and darkmode switch

---
+++ redux/ducks/accountPrefs.js
@@ -0,0 +1,21 @@
+const TOGGLE_DARKMODE = "toggle_darkmode";
+
+export function toggleDarkMode() {
+    return {
+        type: TOGGLE_DARKMODE,
+    };
+}
+
+const initialState = {
+    darkMode: false,
+};
+
+
+export default function accountPrefsReducer(state = initialState, action){
+    switch (action.type) {
+        case TOGGLE_DARKMODE:
+            return{...state, darkMode: !state.darkMode };
+        default:
+            return state;
+    }
+}
--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,18 +1,20 @@
-import React, { useState, useCallback } from "react";
-import { ActivityIndicator, Button, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, Button, StyleSheet, Switch, Text, View } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {useUsername} from "../hooks/useAPI";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signOutAction } from "../redux/ducks/blogAuth";
+import { toggleDarkMode } from "../redux/ducks/accountPrefs";
 
 
 export default function AccountScreen({ navigation }) {
   const [username, loading, error, refresh] = useUsername();
+  const isDarkModeOn = useSelector((state) => state.prefs.darkMode);
   const dispatch = useDispatch();
 
   // signs out if the useUsername hook returns error as true
-  useState(() => {
+  useEffect(() => {
     console.log("Signing out");
     if (error) {
       signOut();
@@ -20,7 +22,7 @@ export default function AccountScreen({ navigation }) {
   }, [error]);
 
     // Check for when we come back to this screen
-  useState(() => {
+  useEffect(() => {
     const removeListener = navigation.addListener("focus", () => {
 
       refresh(true);
@@ -36,12 +38,44 @@ export default function AccountScreen({ navigation }) {
   }
 
   return (
-    <View style={commonStyles.container}>
-      <Text>Account Screen</Text>
-      {loading ? <ActivityIndicator /> : <Text>{username}</Text>}
+    <View style={[
+      commonStyles.container,
+      isDarkModeOn && { backgroundColor: "black" },
+    ]}>
+      <Text style={[styles.titleText,isDarkModeOn && { color: "white" }]}>Account Screen</Text>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text style={styles.usernameText}>{username}</Text>
+      )}
+      <View style={styles.horizontalBlock}>
+        <Text style={isDarkModeOn && { color: "white" }}>Dark mode</Text>
+        <Switch
+          value={isDarkModeOn}
+          onValueChange={() => dispatch(toggleDarkMode())}
+        />
+      </View>
+      <Text> {isDarkModeOn ? "DARK MODE ON" : "DARK MODE OFF"}</Text>
       <Button title="Sign out" onPress={signOut} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  usernameText: {
+    fontSize: 18,
+    color: "grey",
+    marginBottom: 12,
+  },
+  horizontalBlock: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "40%",
+    flexDirection: "row",
+  },
+});
